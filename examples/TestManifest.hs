d3ef5ea5d5cb09b11b555e9b5ec2968f0c22edd1
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-02-19 17:54:27 -0500

    Move Vect/Nat stuff to separate repo

--- examples/TestManifest.hs
+++ examples/TestManifest.hs
@@ -10,6 +10,7 @@ import qualified Data.ByteString as BS
 import Data.List (find)
 import Control.Monad.Trans.State
 import Data.Proxy
+import Data.TypeNat.Vect
 import Manifest.Manifest
 
 data User = User BS.ByteString
@@ -70,7 +71,7 @@ instance Manifest TestManifest where
       list <- get
       return $ case find ((==) bs . fst) list of
         Nothing -> Nothing
-        Just (_, bss) -> case listToVect bss lengthProxy of
+        Just (_, bss) -> case listToVect bss of
           Nothing -> Nothing
           -- TODO Must use an ExceptT so that we can throw a translation error
           -- in case the list is not the right size.
@@ -109,7 +110,7 @@ main = do
   let action2 = read john (Proxy :: Proxy (TestManifest (WithSaltedDigest User)))
   (Right x, tmSaltedDigest') <- manifest tmSaltedDigest action
   (Right y, _) <- manifest tmSaltedDigest' action2
-  (Right z, _) <- manifest tmSaltedDigest action2
+  (Right z, _) <- manifest tmSaltedDigest' action2
   print x
   print y
   print z
