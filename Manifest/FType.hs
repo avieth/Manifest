5d2e511ba2b4a5ff2355a639fc16421fdc26b6b8
Alexander Vieth <alex@lagoa.com>
2015-03-24 11:48:19 -0400

    Comments in FType.hs

--- Manifest/FType.hs
+++ Manifest/FType.hs
@@ -8,8 +8,11 @@ module Manifest.FType (
 
   ) where
 
+-- | Description of a function: either injective or not injective.
 data FType = FInjective | FNotInjective
 
+-- | Expresses the fact that the composition f . g is injective iff both
+--   f and g are injective.
 type family FTypeMeet (m1 :: FType) (m2 :: FType) :: FType where
   FTypeMeet FInjective FInjective = FInjective
   FTypeMeet FNotInjective FInjective = FNotInjective
