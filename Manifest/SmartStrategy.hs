79a128916ae4901e9cf7e9177f5a2446150ed8b9
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2015-03-27 01:58:37 -0400

    Fix race in SmartStrategy
    Rather clumsy, but it works.
    
    The problem: it was possible and likely that a resource would be opened
    twice but one of the handles left unclosed.
    
      x <- function someManifest `at_` True
      y <- function someManifest `at_` False
    
    Running this term, the resource for someManifest would be
    demanded concurrently by both MAt terms; both would inspect the map and
    see that nothing is there, so both would acquire a resource, and whoever
    took longer would have the last word and place his resource in the map.
    The previously acquired one would go unclosed!

--- Manifest/SmartStrategy.hs
+++ Manifest/SmartStrategy.hs
@@ -21,6 +21,7 @@ module Manifest.SmartStrategy (
 
   ) where
 
+import Control.Concurrent.STM (retry)
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Trans.Class
@@ -83,50 +84,87 @@ assureResource
   => m ftype access domain range
   -> SCS.SafeConcurrentState StrategyState (Resource (ResourceType (ManifestResourceDescriptor m ftype access domain range)))
 assureResource m = do
-    map <- SCS.get
+    -- What we really want:
+    --   let rd = resourceDescriptor m
+    --   let key = Identity rd
+    --   map <- SCS.get
+    --   resrc <- getOrInsert rd (acquireResource rd) map
+    --   SCS.set map
+    -- As you can see, the SCS state is irrelevant, since the real mutable
+    -- action happens inside getOrInsert. SafeConcurrentState becomes useless
+    -- here; it's a SafeConcurrenetDependentMap that we need!
+    -- Yes, a thread- and exception-safe mutable dependent map would be ideal,
+    -- and we could just take it from a reader. If the computation blows up,
+    -- the map given to the reader is still there for inspection; accomplishes
+    -- the same thing that SafeConcurrentState does
     let rd = resourceDescriptor m
     let key = Identity rd
     -- ^ Keys in the resource map are Identity functor values!
-    case DM.lookup key map of
-      Just resrc -> return resrc
-      Nothing -> do
-        resrc <- SCS.embedIO $ acquireResource rd
-        -- DANGER! Somebody else could try to acquire the resource
-        -- concurrently. Will this be a problem!?
-        SCS.modify (updateMap key resrc)
-        -- DANGER! This may overwrite something! Will this be a problem?
-        -- Yes it will !!! The old resource will never be released. So, we
-        -- have a problem that STM cannot solve.
-        -- Maybe we could put an "acquiring..." into the map
-        -- atomically if it's not there. After acquisition, we replace it
-        -- with the resource. But what do we do if we find "acquiring..."
-        -- in there? Just wait? We'd want an MVar in any case. The MVar
-        -- signals that "Someone is trying to acquire a resource for this
-        -- descriptor, please wait".
-        -- Hm, could we build that on top of the dependent map?
-        -- ConcurrentDependentMap?
-        return resrc
-  where
-    updateMap key resrc map = ((), DM.insert key resrc map)
 
-type StrategyState = DM.DependentMap DResourceMap DResourceKey Resource
+    -- We have atomic access to the resource map, but during an STM transaction
+    -- with that map, we are incapable of acquiring a resource because that's
+    -- IO! Nevertheless, we must somehow ensure that we never attempt to
+    -- acquire a resource for the same descriptor twice. To this end, we
+    -- atomically modify the map with a return value:
+    --   - if nothing is there, place a Pending, then later we'll acquire the
+    --     resource a put in a Present value.
+    --   - if Pending, retry. We expect some other thread to replace it with
+    --     Present soon.
+    --   - if Present, take the resource and use it.
+    resourceState <- SCS.modifySTM $ \map -> do
+        case DM.lookup key map of
+          Just (Present resrc) -> return (Present resrc, map)
+          Just Pending -> retry
+          -- TBD seems like a wart, exposing modifySTM and demanding users
+          -- actually work with STM. Is there a better way?
+          Nothing -> return (Pending, DM.insert key Pending map)
+
+    case resourceState of
+      Present resrc -> return resrc
+      Pending -> do
+          resrc <- SCS.embedIO $ acquireResource rd
+          -- TODO should catch exceptions in acquireResource rd so we
+          -- can modify the map to remove the Pending, but it doesn't really
+          -- matter since in practice we'll abort the entire SCS computation
+          -- anyway.
+          -- Indeed, we wouldn't want to remove resource descriptor from the
+          -- map, else some other thread may try to acquire the resource again,
+          -- and again, and again, and so on, and nothing will ever be
+          -- accomplished.
+          SCS.modify (\map -> ((), DM.insert key (Present resrc) map))
+          return resrc
+
+data ConcurrentResource a = Pending | Present (Resource a)
+
+type StrategyState = DM.DependentMap DResourceMap DResourceKey ConcurrentResource
 
 type DResourceKey = Identity
 
 data DResourceMap
 type instance DM.DependentMapFunction DResourceMap a = ResourceType a
 
-releaseAll :: DM.DependentMap DResourceMap DResourceKey Resource -> IO ()
+releaseAll :: DM.DependentMap DResourceMap DResourceKey ConcurrentResource -> IO ()
 releaseAll dmap = DM.foldWithKey releaseOne (return ()) dmap
   where
-    releaseOne _ res io = io >> release res
+    releaseOne _ res io = case res of
+        Pending -> io
+        -- ^ It is safe to assume here that the resource could not be acquired
+        --   (acquireResource threw an exception) and that it will never be
+        --   acquired, so we can just ignore the Pending.
+        --   See assureResource; if a Pending is inserted, it is updated later
+        --   UNLESS acquireResource blows up.
+        Present res -> io >> release res
 
-commitAll :: DM.DependentMap DResourceMap DResourceKey Resource -> IO ()
+commitAll :: DM.DependentMap DResourceMap DResourceKey ConcurrentResource -> IO ()
 commitAll dmap = DM.foldWithKey commitOne (return ()) dmap
   where
-    commitOne _ res io = io >> commit res
+    commitOne _ res io = case res of
+        Pending -> io
+        Present res -> io >> commit res
 
-rollbackAll :: DM.DependentMap DResourceMap DResourceKey Resource -> IO ()
+rollbackAll :: DM.DependentMap DResourceMap DResourceKey ConcurrentResource -> IO ()
 rollbackAll dmap = DM.foldWithKey rollbackOne (return ()) dmap
   where
-    rollbackOne _ res io = io >> rollback res
+    rollbackOne _ res io = case res of
+        Pending -> io
+        Present res -> io >> rollback res
